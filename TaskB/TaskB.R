65adcb6075465063835b7cd599a9dbe4da3e5fe7
varkolya <132575984+varkolya@users.noreply.github.com>
2023-05-21 13:49:03 +0200

    Update TaskB.R
    Added more code for TaskB 2.3, including code for Question 4

--- TaskB/TaskB.R
+++ TaskB/TaskB.R
@@ -359,3 +359,182 @@ summary(ophtal.mcmc_complex)
 summary(ophtal.mcmc_pred)
 summary(ophtal.mcmc_semicomp_pred)
 summary(ophtal.mcmc_complex_pred)
+
+
+#### 2.3 Prediction with a different approach ####
+my.data <- list(lutein_2003 = c(6.4, 7.5, 8.4, 9.6, 12.0, 4.2, 3.1, 6.3, 4.4, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                lutein_1999 = c(3.5, 2.9, 4.1, 5.1, 6.4, 1.9, 1.3, 4.1, 2.3, 5, 5, 2.3, 4.1, 1, 2, 3, 4, 5, 20),
+                N = 19)
+
+# Parameters to monitor
+parameters <- c("beta0", "beta1", "sigma2", "lutein_2003[10:19]")
+
+# Running JAGS:
+jags <- jags.model(file ="ophtalmology_model_semicomp.txt",
+                   data = my.data,
+                   inits = my.inits,
+                   n.chains = 2)
+update(jags,2000)
+ophtal.sim <- coda.samples(model = jags,
+                           variable.names = parameters,
+                           n.iter = 90000, 
+                           thin = 10)
+
+ophtal.mcmc_complex_pred_v2 <- as.mcmc.list(ophtal.sim)
+
+# Produce general summary of obtained MCMC sampling
+summary(ophtal.mcmc_complex_pred_v2)
+
+
+#### 2.3 Prediction with a different approach v2 ####
+cat("model
+  {
+    for (i in 1:N){
+      lutein_2003[i] ~ dnorm(mu[i],tau)
+      mu[i] <- beta0 + beta1*(lutein_1999[i]-mean(lutein_1999[]))
+    }
+    sigma2 <- 1/tau
+    beta0 ~ dnorm(0,1.0E-6)
+    beta1 ~ dnorm(0,1.0E-6)
+    tau ~ dgamma(1.0E-3,1.0E-3)
+    
+    # Prediction for new 1999 values
+    for (j in 1:N_new) {
+      y_new[j] ~ dnorm(mu_new[j], tau)
+      mu_new[j] <- beta0 + beta1*(x_new[j]-mean(lutein_1999[]))
+    }
+  }", file="ophtalmology_model_complex_pred_v2.txt")
+
+# Define new data
+N_new <- 10
+x_new <- c(5, 5, 2.3, 4.1, 1, 2, 3, 4, 5, 20)
+
+# Combine new data with original data
+my.data <- list(lutein_2003 = c(6.4, 7.5, 8.4, 9.6, 12.0, 4.2, 3.1, 6.3, 4.4),
+                lutein_1999 = c(3.5, 2.9, 4.1, 5.1, 6.4, 1.9, 1.3, 4.1, 2.3),
+                N = 9,
+                y_new = rep(NA, N_new),
+                x_new = x_new,
+                N_new = N_new)
+
+# Parameters to monitor
+parameters <- c("beta0", "beta1", "sigma2", "y_new")
+
+# Running JAGS:
+jags <- jags.model(file ="ophtalmology_model_complex_pred_v2.txt",
+                   data = my.data,
+                   inits = my.inits,
+                   n.chains = 2)
+update(jags,2000)
+ophtal.sim <- coda.samples(model = jags,
+                           variable.names = parameters,
+                           n.iter = 90000, 
+                           thin = 10)
+
+ophtal.mcmc_complex_pred_v3 <- as.mcmc.list(ophtal.sim)
+
+# Produce general summary of obtained MCMC sampling
+summary(ophtal.mcmc_complex_pred_v3)
+
+# Extract posterior samples
+sample_chain1 <- as.data.frame(ophtal.sim[[1]])
+sample_chain2 <- as.data.frame(ophtal.sim[[2]])
+posterior <- rbind(sample_chain1, sample_chain2)
+
+
+# Initialize vector to store posterior probabilities
+posterior_probs <- numeric(N_new)
+
+# Calculate posterior probability for each new observation
+for (j in 1:N_new) {
+  posterior_probs[j] <- mean(posterior[, 3 + j] > 5, na.rm = TRUE)
+}
+
+# Output posterior probabilities
+posterior_probs
+
+
+#### 2.3 Question 4 ####
+cat("model
+  {
+    for (i in 1:N){
+      lutein_2003[i] ~ dnorm(mu[i],tau)
+      mu[i] <- beta0 + beta1*(lutein_1999[i]-mean(lutein_1999[]))
+    }
+    sigma2 <- 1/tau
+    beta0 ~ dnorm(0,1.0E-6)
+    beta1 ~ dnorm(0,1.0E-6)
+    tau ~ dgamma(1.0E-3,1.0E-3)
+    
+    # Prediction for new 1999 values
+    for (j in 1:N_new) {
+      y_new[j] ~ dnorm(mu_new[j], tau)
+      mu_new[j] <- beta0 + beta1*(x_new[j]-mean(lutein_1999[]))
+    }
+  }", file="ophtalmology_model_complex_pred_new_data.txt")
+
+# Define new data
+N_new <- 100
+x_new <- ophtalmology$x
+
+# Combine new data with original data
+my.data <- list(lutein_2003 = c(6.4, 7.5, 8.4, 9.6, 12.0, 4.2, 3.1, 6.3, 4.4),
+                lutein_1999 = c(3.5, 2.9, 4.1, 5.1, 6.4, 1.9, 1.3, 4.1, 2.3),
+                N = 9,
+                y_new = rep(NA, N_new),
+                x_new = x_new,
+                N_new = N_new)
+
+# Parameters to monitor
+parameters <- c("beta0", "beta1", "sigma2", "y_new")
+
+# Running JAGS:
+jags <- jags.model(file ="ophtalmology_model_complex_pred_new_data.txt",
+                   data = my.data,
+                   inits = my.inits,
+                   n.chains = 2)
+update(jags,2000)
+ophtal.sim <- coda.samples(model = jags,
+                           variable.names = parameters,
+                           n.iter = 90000, 
+                           thin = 10)
+
+ophtal.mcmc_complex_pred_new_data <- as.mcmc.list(ophtal.sim)
+
+# Produce general summary of obtained MCMC sampling
+summary(ophtal.mcmc_complex_pred_new_data)
+
+# Extract posterior samples
+sample_chain1 <- as.data.frame(ophtal.sim[[1]])
+sample_chain2 <- as.data.frame(ophtal.sim[[2]])
+posterior <- rbind(sample_chain1, sample_chain2)
+
+
+# Initialize vector to store posterior probabilities
+posterior_probs <- numeric(N_new)
+
+# Calculate posterior probability for each new observation
+for (j in 1:N_new) {
+  posterior_probs[j] <- mean(posterior[, 3 + j] > 5, na.rm = TRUE)
+}
+
+# Output posterior probabilities
+posterior_probs
+
+# Histogram
+hist(posterior_probs)
+
+# b)
+# Number of participants who have a high posterior probability that they took serum-lutein capsules
+sum(posterior_probs > 0.8)
+
+# Calculate posterior probability for each new observation
+for (j in 1:N_new) {
+  posterior_probs[j] <- mean(posterior[, 3 + j] <= 5, na.rm = TRUE)
+}
+
+# Output posterior probabilities
+posterior_probs
+
+# Number of participants who have a high posterior probability that they took serum-lutein capsules
+sum(posterior_probs > 0.8)
