5b042c5d57485e41219506ccd014cdd81559ed88
varkolya <132575984+varkolya@users.noreply.github.com>
2023-05-30 16:34:13 +0200

    Cleaned, structured TaskB_Code.R
    Cleaned and structured the code better, removed some unneccesery codes, etc.

--- TaskB/TaskB_Code.R
+++ TaskB/TaskB_Code.R
@@ -5,10 +5,6 @@ library(readr)
 library(runjags)
 
 
-#### 2.3 simple model (Question 1-2) ####
-ophtalmology <- read_csv("ophtalmology.txt")
-ophtalmology <- ophtalmology[2]    # Drop first column as it is only an index column
-
 # Prepare data:
 my.data <- list(lutein_2003 = c(6.4, 7.5, 8.4, 9.6, 12.0, 4.2, 3.1, 6.3, 4.4),
                 lutein_1999 = c(3.5, 2.9, 4.1, 5.1, 6.4, 1.9, 1.3, 4.1, 2.3),
@@ -16,10 +12,11 @@ my.data <- list(lutein_2003 = c(6.4, 7.5, 8.4, 9.6, 12.0, 4.2, 3.1, 6.3, 4.4),
 
 # Initial parameters
 my.inits <- list(
-  list(beta0 = 0.1, beta1 = 0.025, tau=1/0.05),
+  list(beta0 = 0.1, beta1 = 0.025, tau = 1/0.05),
   list(beta0 = 0.9, beta1 = 0.6, tau = 1/0.3)
 )
 
+#### 2.3 simple model (Question 1-2) ####
 # Model specification
 cat("model
   {
@@ -37,7 +34,7 @@ cat("model
 parameters <- c("beta0", "beta1", "sigma2")
 
 # Running JAGS:
-jags <- jags.model(file ="ophtalmology_model.txt",
+jags <- jags.model(file = "ophtalmology_model.txt",
                    data = my.data,
                    inits = my.inits,
                    n.chains = 2)
@@ -47,7 +44,7 @@ ophtal.sim <- coda.samples(model = jags,
                            n.iter = 5000, 
                            thin = 1)
 
-# Convert osteo.sim into mcmc.list for processing with CODA package
+# Convert ophtal.sim into mcmc.list for processing with CODA package
 ophtal.mcmc <- as.mcmc.list(ophtal.sim)
 
 # Produce general summary of obtained MCMC sampling
@@ -68,7 +65,6 @@ traceplot(ophtal.mcmc[,3], main = '"Zoomed in" trace of sigma2', cex.main = 1.5,
 # Autocorrelation plot
 par(mfrow=c(1,1))
 acfplot(ophtal.mcmc, lag.max = 30, ylim = c(-0.2, 1))
-# crosscorr.plot(ophtal.mcmc)
 
 # Gelman-Rubin convergence diagnostics
 gelman.diag(ophtal.mcmc)
@@ -78,25 +74,8 @@ gelman.plot(ophtal.mcmc[,3], main = 'sigma2', ask = FALSE)
 
 
 #### 2.3 Longer model with thinning, burnin (Question 2) ####
-# Only barely mention this in the Assignment --> what's important is the next model
-cat("model
-  {
-    for (i in 1:N){
-      lutein_2003[i] ~ dnorm(mu[i],tau)
-      mu[i] <- beta0 + beta1*(lutein_1999[i])
-    }
-    sigma2 <- 1/tau
-    beta0 ~ dnorm(0,1.0E-6)
-    beta1 ~ dnorm(0,1.0E-6)
-    tau ~ dgamma(1.0E-3,1.0E-3)
-  }", file="ophtalmology_model_semicomp.txt")
-
-
-# Parameters to monitor
-parameters <- c("beta0", "beta1", "sigma2")
-
-# Running JAGS:
-jags <- jags.model(file ="ophtalmology_model_semicomp.txt",
+# Running JAGS (with the same model as previously):
+jags <- jags.model(file = "ophtalmology_model.txt",
                    data = my.data,
                    inits = my.inits,
                    n.chains = 2)
@@ -106,7 +85,7 @@ ophtal.sim <- coda.samples(model = jags,
                            n.iter = 90000, 
                            thin = 10)
 
-# Convert osteo.sim into mcmc.list for processing with CODA package
+# Convert ophtal.sim into mcmc.list for processing with CODA package
 ophtal.mcmc_semicomp <- as.mcmc.list(ophtal.sim)
 
 # Produce general summary of obtained MCMC sampling
@@ -122,14 +101,13 @@ densplot(ophtal.mcmc_semicomp)
 # Autocorrelation plot
 par(mfrow=c(1,1))
 acfplot(ophtal.mcmc_semicomp)
-# crosscorr.plot(ophtal.mcmc_semicomp)
 
 # Gelman-Rubin convergence diagnostics
 gelman.diag(ophtal.mcmc_semicomp)
 gelman.plot(ophtal.mcmc_semicomp,ask=FALSE)
 
 
-#### 2.3 Longer model with thinning, burnin, centering ####
+#### 2.3 Longer model with thinning, burnin, centering (Question 2) ####
 cat("model
   {
     for (i in 1:N){
@@ -157,7 +135,7 @@ ophtal.sim <- coda.samples(model = jags,
                            n.iter = 90000, 
                            thin = 10)
 
-# Convert osteo.sim into mcmc.list for processing with CODA package
+# Convert ophtal.sim into mcmc.list for processing with CODA package
 ophtal.mcmc_complex <- as.mcmc.list(ophtal.sim)
 
 # Produce general summary of obtained MCMC sampling
@@ -178,11 +156,12 @@ traceplot(ophtal.mcmc_complex[,3], main = '"Zoomed in" trace of sigma2', cex.mai
 # Autocorrelation plot
 par(mfrow=c(1,1))
 acfplot(ophtal.mcmc_complex)
-# crosscorr.plot(ophtal.mcmc_complex)
 
 # Gelman-Rubin convergence diagnostics
 gelman.diag(ophtal.mcmc_complex)
-gelman.plot(ophtal.mcmc_complex,ask=FALSE)
+gelman.plot(ophtal.mcmc_complex[,1], main = 'beta0', ask = FALSE)
+gelman.plot(ophtal.mcmc_complex[,2], main = 'beta1', ask = FALSE)
+gelman.plot(ophtal.mcmc_complex[,3], main = 'sigma2', ask = FALSE)
 
 
 #### 2.3 Complex Model with prediction (Question 3) ####
@@ -216,7 +195,7 @@ ophtal.sim <- coda.samples(model = jags,
                            n.iter = 90000, 
                            thin = 10)
 
-# Convert osteo.sim into mcmc.list for processing with CODA package
+# Convert ophtal.sim into mcmc.list for processing with CODA package
 ophtal.mcmc_complex_pred <- as.mcmc.list(ophtal.sim)
 
 # Produce general summary of obtained MCMC sampling
@@ -226,6 +205,9 @@ HPDinterval(ophtal.combined)
 
 
 #### 2.3 Predicting for 100 participants (Question 4) ####
+ophtalmology <- read_csv("ophtalmology.txt")
+ophtalmology <- ophtalmology[2]    # Drop first column as it is only an index column
+
 cat("model
   {
     for (i in 1:N){
@@ -283,7 +265,7 @@ posterior <- rbind(sample_chain1, sample_chain2)
 # Initialize vector to store posterior probabilities
 posterior_probs <- numeric(N_new)
 
-# Calculate posterior probability for each new observation
+# Calculate posterior probability of corrected serum-lutein >5 for each new observation
 for (j in 1:N_new) {
   posterior_probs[j] <- mean(posterior[, 3 + j] > 5, na.rm = TRUE)
 }
@@ -291,14 +273,14 @@ for (j in 1:N_new) {
 # Output posterior probabilities
 posterior_probs
 
-# Histogram
+# Histogram of these posterior predictive probabilities - a) sub-question
 hist(posterior_probs)
 
-# b)
+# b) sub-question
 # Number of participants who have a high posterior probability that they took serum-lutein capsules
 sum(posterior_probs > 0.8)
 
-# Calculate posterior probability for each new observation
+# Calculate posterior probability of corrected serum-lutein <=5 for each new observation
 for (j in 1:N_new) {
   posterior_probs[j] <- mean(posterior[, 3 + j] <= 5, na.rm = TRUE)
 }
